846434af9d937e177b74c5fbbd090bcca9aa2cd4
sscnt <pixeltc@gmail.com>
2014-04-29 17:02:55 +0900

    Initial Commit

---
+++ Pastel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pastel
+//
+//  Created by SSC on 2014/04/29.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Pastel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pastel
+//
+//  Created by SSC on 2014/04/29.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
