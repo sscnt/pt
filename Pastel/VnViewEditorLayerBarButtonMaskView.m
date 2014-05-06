f1c7bed65d7b197751d32fee96c5d62d07f2f011
sscnt <pixeltc@gmail.com>
2014-05-06 18:35:44 +0900

    e

--- Pastel/VnViewEditorLayerBarButtonMaskView.m
+++ Pastel/VnViewEditorLayerBarButtonMaskView.m
@@ -36,39 +36,24 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextClearRect(context, rect);
     
-    //マスクするパスを作成。
-    /*
-    CGContextBeginPath(context);
-    CGContextMoveToPoint(context, 0.0f, 0.0f);
-    CGContextAddLineToPoint(context, rect.size.width, 0.0f);
-    CGContextAddLineToPoint(context, rect.size.width, rect.size.height);
-    CGContextAddLineToPoint(context, 0.0f, rect.size.height);
-    CGContextClosePath(context);
-     */
-    CGContextAddRect(context, rect);
     
-    float p = (rect.size.width - _radius * 2.0f) / 2.0f;
-    float t = (rect.size.height - _radius * 2.0f) / 2.0f;
-    UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(p, t, _radius * 2.0f, _radius * 2.0f)];
-    ovalPath = [ovalPath bezierPathByReversingPath];
-    [ovalPath addClip];
     
-    CGContextClip(context);
+    float p = rect.size.width / 2.0f;
+    float t = rect.size.height / 2.0f;
+    CGContextAddArc(context, p, t, _radius, 0.0f, M_PI * 2.0f, 1);
     
-    //中抜きの場合はこっち。
-    //CGContextAddRect (context, self.bounds);
-    //CGContextEOClip(context);
+    CGContextAddRect (context, self.bounds);
+    CGContextEOClip(context);
     
-    //ここから以下に描画したものがマスク対象になる。
-    const CGFloat *c = CGColorGetComponents(_maskColor.CGColor);
-    CGContextSetRGBFillColor(context, c[0], c[1], c[2], 1.0f);
-    CGContextFillRect(context, CGRectMake(0, 0, rect.size.width, rect.size.height));
+    UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRect: rect];
+    [_maskColor setFill];
+    [rectanglePath fill];
     
     if (_selected) {
         float r = _radius + 3.0f;
         p = (rect.size.width - r * 2.0f) / 2.0f;
         t = (rect.size.height - r * 2.0f) / 2.0f;
-        ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(p, t, r * 2.0f, r * 2.0f)];
+        UIBezierPath* ovalPath = [UIBezierPath bezierPathWithOvalInRect: CGRectMake(p, t, r * 2.0f, r * 2.0f)];
         [_selectionColor setStroke];
         ovalPath.lineWidth = 2;
         [ovalPath stroke];
